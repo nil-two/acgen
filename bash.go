8f7106d12c0398fac1f4843d22ba6d638073449b
kusabashira <kusabashira227@gmail.com>
2015-12-31 22:05:44 +0900

    Add completion-generator for bash

---
+++ bash.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"io"
+	"text/template"
+)
+
+func init() {
+	Generators["bash"] = GenerateBashCompletion
+}
+
+type Bash struct {
+	Name string
+	Opts []string
+}
+
+func NewBash(c *Config) (b *Bash, err error) {
+	b = new(Bash)
+	b.Name = c.Name
+	for _, flag := range c.Flags {
+		for _, long := range flag.Long {
+			opt := "--" + long
+			if flag.Arg != "" {
+				opt += "="
+			}
+			b.Opts = append(b.Opts, opt)
+		}
+	}
+	return b, nil
+}
+
+var BashCompletionTemplateText = `
+_{{.Name}}()
+{
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  local opts="{{range .Opts}}
+    {{.}}{{end}}
+  "
+  case "$cur" in
+    -*)
+      COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+      ;;
+    *)
+      _filedir
+      ;;
+  esac
+  [[ ${COMPREPLY[0]} == *= ]] && compopt -o nospace
+}
+complete -F _{{.Name}} {{.Name}}
+`[1:]
+
+func GenerateBashCompletion(w io.Writer, c *Config) error {
+	tmpl, err := template.New("bash").Parse(BashCompletionTemplateText)
+	if err != nil {
+		return err
+	}
+	b, err := NewBash(c)
+	if err != nil {
+		return err
+	}
+	return tmpl.Execute(w, b)
+}
