303f98e0d9aa46e1cad185c4762e9690c8ac66bd
kusabashira <kusabashira227@gmail.com>
2016-02-07 20:25:05 +0900

    Declare Command before Flag

--- generator.go
+++ generator.go
@@ -6,16 +6,16 @@ import (
 	"sync"
 )
 
+type Command struct {
+	Name  string
+	Flags []*Flag
+}
+
 type Flag struct {
 	Short       []string
 	Long        []string
 	Arg         string
 	Description string
-}
-
-type Command struct {
-	Name  string
-	Flags []*Flag
 }
 
 type Generator func(w io.Writer, c *Command) error
